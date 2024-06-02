fc5d8d3f1c815080c6a419b2d973f04947f7534d
Inari Goro <qqnq7um9k@gmail.com>
2024-06-02 21:55:04 +0900

    updated ASCII Checker

--- ASCII_checker.c
+++ ASCII_checker.c
@@ -3,41 +3,36 @@
 
 int main(void){
 	char character;
-	char list[52];
+	char response;
 	
-	for(int i = 0; i < 26; i++){
-		list[i] = 'A' + i;
-	}
-	
-	for(int i = 0; i < 26; i++){
-		list[26 + i] = 'a' + i;
-	}
 	printf("Welcome to ASCII code checker!\n");
-	printf("note : only first input alphabet can be loaded, even if you enter 2 or more alphabets.\n\n");
-	printf("Enter a character whose ASCII code you'd like to know: ");
+	printf("note: only the first input character will be considered, even if you enter 2 or more characters.\n\n");
 	
-	while(scanf("%c", &character) != 1 || !isalpha(character)){
-		printf("Enter only alphabetical character: ");
-		while(getchar() != '\n');
-	}
-	
-	int flg = 0;
-	
-	for(int i = 0; i < 52; i++){
-		if(character == list[i] && (character >= 'A' && character <= 'Z')){
-			printf("ASCII code of the character you entered is %d.\n", i + 65);
-			flg = 1;
-			break;
-		} else if(character == list[i] && (character >= 'a' && character <= 'z')){
-			printf("ASCII code of the character you entered is %d.\n", i + 71);
-			flg = 1;
-			break;
-		} 
-	}
+	do{
+		printf("Enter a character whose ASCII code you'd like to know: ");
 		
-	if(!flg){
-		printf("The character you entered is NOT included to alphabetical ASCII code.\n");
-	}
+		character = getchar();
+		while(getchar() != '\n');
+		
+		if(isascii(character) && isprint(character)){
+			printf("ASCII code of the character you entered is %d.\n", character);
+		} else {
+			printf("ERROR: Non-ASCII or Non-printable character entered!\n");
+			continue;
+		}
+		
+		do{
+		
+			printf("Do you want to check another character? (y/n): ");
+			
+			response = getchar();
+			
+			while(getchar() != '\n');
+		} while(response != 'y' && response != 'n');
+		
+	} while(response == 'y');
+	
+	printf("Good bye!\n");
 	
 	return 0;
 }
